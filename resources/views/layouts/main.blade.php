95df9cb59891fd319661e050f474414ee7095bae
andreaugustos-sourc <andreaugusto1033@gmail.com>
2024-04-17 02:47:09 -0300

    feat: details

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,14 +10,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script defer src="/js/script.js"></script>
 </head>
 
 <body>
     @if (session('msg'))
-        <div class="alert alert-success">
-            {{ session('msg') }}
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <span>{{ session('msg') }}</span>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
+
     @endif
     <nav class="navbar navbar-dark bg-dark">
         <div class="container-fluid justify-content-evenly">
